7c4c2e31726ca8485f919621a1d0c9eae319bea5
3x3cut0r <executor55@gmx.de>
2021-12-17 18:13:52 +0100

    getFirebogLists.sh 0.01

---
+++ scripts/getFirebogLists.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Author:   Julian Reith
+# E-Mail:   julianreith@gmx.de
+# Version:  0.01
+# Date:     2021-12-17
+#
+# Description:
+#  this script pulls the adlists from https://firebog.net and stores them 
+#  in separate lists named by category and type (tick, nocross, all)
+#
+
+### GLOBAL VARS ###
+listDir=/lists
+
+
+### FUNCTIONS ###
+function createListDir() {
+    mkdir -p $listDir
+}
+
+### START OF SCRIPT ###
+createListDir
