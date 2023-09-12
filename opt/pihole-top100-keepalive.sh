8cdea038523fc91c01aac7e8191161b0ebaee4c6
3x3cut0r <executor55@gmx.de>
2023-09-12 17:41:23 +0200

    Update pihole-top100-keepalive.sh

--- opt/pihole-top100-keepalive.sh
+++ opt/pihole-top100-keepalive.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
 while true; do
-    # clean up domains-top100.txt
-    > /etc/pihole/domains-top100.txt
-    
-    # add custom domains from keepalive.txt
-    if [ -f /etc/pihole/domains-keepalive.txt ]; then
-        cat /etc/pihole/domains-keepalive.txt > /etc/pihole/domains-top100.txt
+    if [ -f /etc/pihole/domains-keepalive.txt ]; 
+    then
+         # add custom domains from keepalive.txt
+        cat /etc/pihole/domains-keepalive.txt > /etc/pihole/domains.txt
+    else
+        # clean up domains.txt
+        > /etc/pihole/domains.txt
     fi
 
     # add the 100 top permitted domains from pihole
-    sqlite3 "/etc/pihole/pihole-FTL.db" "SELECT domain,count(domain) FROM queries WHERE (STATUS == 2 OR STATUS == 3) GROUP BY domain ORDER BY count(domain) DESC LIMIT 100" | cut -d "|" -f 1 >> /etc/pihole/domains-top100.txt
-
+    sqlite3 "/etc/pihole/pihole-FTL.db" "SELECT domain,count(domain) FROM queries WHERE (STATUS == 2 OR STATUS == 3) GROUP BY domain ORDER BY count(domain) DESC LIMIT 100" | cut -d "|" -f 1 > /etc/pihole/domains-top100.txt
+    cat /etc/pihole/domains-top100.txt >> /etc/pihole/domains.txt
+    
     # get current cache-min-ttl from unbound
     CACHE_MIN_TTL=$(grep "cache-min-ttl" /etc/unbound/unbound.conf.d/pi-hole.conf | awk '{print $2}')
     TOTAL_PAUSE=$((CACHE_MIN_TTL / 2))
     
-    # request all domains in the domains-top100.txt to renew ttl from unbound
-    for domain in $(cat /etc/pihole/domains-top100.txt); do
+    # request all domains in the domains.txt to renew ttl from unbound
+    for domain in $(cat /etc/pihole/domains.txt); do
        dig @127.0.0.1 -p 5335 $domain # request to unbound directly -> does not falsify the top permitted domain list
        sleep 1 # sleep 1 to not ratelimit root dns server
     done
